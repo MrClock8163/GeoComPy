9568aa43d9f31ee115b1e1d8887d64f6fcd6edb1
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 01:16:50 +0200

    TPS1200PCOM documentation

--- docs/api/tps1200p/com.rst
+++ docs/api/tps1200p/com.rst
@@ -6,3 +6,5 @@ geocompy.tps1200p.com
 
 .. autoclass:: TPS1200PCOM
     :members:
+    :undoc-members:
+    :class-doc-from: both
--- src/geocompy/tps1200p/com.py
+++ src/geocompy/tps1200p/com.py
@@ -1,3 +1,15 @@
+"""
+``geocompy.tps1200p.com``
+=========================
+
+Definitions for the TPS1200+ Communication subsystem.
+
+Types
+-----
+
+- ``TPS1200PCOM``
+
+"""
 from __future__ import annotations
 
 from enum import Enum
@@ -10,9 +22,29 @@ from ..data import toenum
 
 
 class TPS1200PCOM(GeoComSubsystem):
+    """
+    Communication subsystem of the TPS1200+ GeoCom protocol.
+
+    This subsystem contains functions relevant to the communication
+    with the instrument.
+
+    """
     class STOPMODE(Enum):
         @classmethod
         def parse(cls, value: str) -> TPS1200PCOM.STOPMODE:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~TPS1200PCOM.STOPMODE
+                Parsed enum member.
+            """
             return cls(int(value))
 
         SHUTDOWN = 0
@@ -21,12 +53,42 @@ class TPS1200PCOM(GeoComSubsystem):
     class STARTUPMODE(Enum):
         @classmethod
         def parse(cls, value: str) -> TPS1200PCOM.STARTUPMODE:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~TPS1200PCOM.STARTUPMODE
+                Parsed enum member.
+            """
             return cls(int(value))
 
         LOCAL = 0
         REMOTE = 1
 
     def get_sw_version(self) -> GeoComResponse:
+        """
+        RPC 110, ``COM_GetSWVersion``
+
+        Gets the version of the installed GeoCom release.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **release** (`int`): Release number.
+                - **version** (`int`): Version number.
+                - **subversion** (`int`): Subversion number.
+
+        See Also
+        --------
+        csv.get_sw_version
+        """
         return self._request(
             110,
             parsers={
@@ -38,8 +100,33 @@ class TPS1200PCOM(GeoComSubsystem):
 
     def switch_on(
         self,
-        onmode: STARTUPMODE | str = STARTUPMODE.LOCAL
+        onmode: STARTUPMODE | str = STARTUPMODE.REMOTE
     ) -> GeoComResponse:
+        """
+        RPC 111, ``COM_SwitchOnTPS``
+
+        Switches on the instrument.
+
+        Parameters
+        ----------
+        onmode : STARTUPMODE | str, optional
+            Desired startup mode, by default STARTUPMODE.REMOTE
+
+        Returns
+        -------
+        GeoComResponse
+            - Error codes:
+                - ``NOT_IMPL``: Instrument is already on.
+        
+        Notes
+        -----
+        The instrument can be switched on with any command, or even just
+        a single character.
+
+        See Also
+        --------
+        switch_off
+        """
         _onmode = toenum(self.STARTUPMODE, onmode)
         return self._request(
             111,
@@ -50,6 +137,24 @@ class TPS1200PCOM(GeoComSubsystem):
         self,
         offmode: STOPMODE | str = STOPMODE.SHUTDOWN
     ) -> GeoComResponse:
+        """
+        RPC 112, ``COM_SwitchOffTPS``
+
+        Switches off the instrument.
+
+        Parameters
+        ----------
+        offmode : STOPMODE | str, optional
+            Desired stop mode, by default STOPMODE.SHUTDOWN
+
+        Returns
+        -------
+        GeoComResponse
+
+        See Also
+        --------
+        switch_on
+        """
         _offmode = toenum(self.STOPMODE, offmode)
         return self._request(
             112,
@@ -57,9 +162,30 @@ class TPS1200PCOM(GeoComSubsystem):
         )
 
     def nullproc(self) -> GeoComResponse:
+        """
+        RPC 0, ``COM_NullProc``
+
+        Tests connection by executing the null process.
+
+        """
         return self._request(0)
 
     def get_binary_available(self) -> GeoComResponse:
+        """
+        RPC 113, ``COM_GetBinaryAvailable``
+
+        Checks if the instrument supports binary communication.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **available** (`bool`): Availability of binary mode.
+
+        See Also
+        --------
+        set_binary_available
+        """
         return self._request(
             113,
             parsers={
@@ -69,9 +195,23 @@ class TPS1200PCOM(GeoComSubsystem):
 
     def set_binary_available(
         self,
-        available: bool
+        enable: bool
     ) -> GeoComResponse:
+        """
+        RPC 114, ``COM_SetBinaryAvailable``
+
+        Enables or disables binary communication with the instrument.
+
+        Parameters
+        ----------
+        enable : bool
+            Enable or disable binary communication.
+
+        See Also
+        --------
+        get_binary_available
+        """
         return self._request(
             114,
-            [available]
+            [enable]
         )
