9f665706de301cc54165cb998da1d50445172f2b
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 11:35:44 +0200

    TPS1200PFTR documentation

--- docs/api/tps1200p/ftr.rst
+++ docs/api/tps1200p/ftr.rst
@@ -6,3 +6,5 @@ geocompy.tps1200p.ftr
 
 .. autoclass:: TPS1200PFTR
     :members:
+    :undoc-members:
+    :class-doc-from: both
--- src/geocompy/tps1200p/ftr.py
+++ src/geocompy/tps1200p/ftr.py
@@ -1,3 +1,15 @@
+"""
+``geocompy.tps1200p.ftr``
+=========================
+
+Definitions for the TPS1200+ File transfer subsystem.
+
+Types
+-----
+
+- ``TPS1200PFTR``
+
+"""
 from __future__ import annotations
 
 from enum import Enum
@@ -15,9 +27,29 @@ from ..data import (
 
 
 class TPS1200PFTR(GeoComSubsystem):
+    """
+    File transfer subsystem of the TPS1200+ GeoCom protocol.
+
+    This subsystem provides access to the internal file system of the
+    instrument, and provides methods to list or download files.
+
+    """
     class DEVICETYPE(Enum):
         @classmethod
         def parse(cls, value: str) -> TPS1200PFTR.DEVICETYPE:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~TPS1200PFTR.DEVICETYPE
+                Parsed enum member.
+            """
             return cls(int(value))
 
         INTERNAL = 0
@@ -26,6 +58,19 @@ class TPS1200PFTR(GeoComSubsystem):
     class FILETYPE(Enum):
         @classmethod
         def parse(cls, value: str) -> TPS1200PFTR.FILETYPE:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~TPS1200PFTR.FILETYPE
+                Parsed enum member.
+            """
             return cls(int(value))
 
         UNKNOWN = 0  # ?
@@ -35,8 +80,39 @@ class TPS1200PFTR(GeoComSubsystem):
         self,
         device: DEVICETYPE | str = DEVICETYPE.PCPARD,
         filetype: FILETYPE | str = FILETYPE.UNKNOWN,
-        path: str = "/root"
+        path: str = ""
     ) -> GeoComResponse:
+        """
+        RPC 23306, ``FTR_SetupList``
+
+        Prepares file listing of the specified file type, on the selected
+        memory device.
+
+        Parameters
+        ----------
+        device : DEVICETYPE | str, optional
+            Memory device, by default PCPARD
+        filetype : FILETYPE | str, optional
+            File type, by default UNKNOWN
+        path : str, optional
+            Search path, by default ""
+
+        Returns
+        -------
+        GeoComResponse
+            - Error codes:
+                - ``IVPARAM``: Memory device unavailable, or cannot find
+                  search path.
+                - ``NOTOK``: Setup already exists, previous setup was not
+                  aborted.
+                - ``FTR_FILEACCESS``: File access error.
+        
+        See Also
+        --------
+        list
+        abort_list
+
+        """
         _device = toenum(self.DEVICETYPE, device)
         _filetype = toenum(self.FILETYPE, filetype)
         return self._request(
@@ -48,6 +124,38 @@ class TPS1200PFTR(GeoComSubsystem):
         self,
         next: bool = False
     ) -> GeoComResponse:
+        """
+        RPC 23307, ``FTR_List``
+
+        Gets a single file entry according to the predefined listing
+        setup.
+
+        Parameters
+        ----------
+        next : bool, optional
+            Get the next item, after a previous call (get first item
+            otherwise), by default False
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **last** (`bool`): If file is last in listing.
+                - **filename** (`str`): Name of the file.
+                - **size** (`int`): File size [byte].
+                - **modified** (`datetime` | None): Date and time of last
+                  modification.
+            - Error codes:
+                - ``FTR_MISSINGSETUP``: No active listing setup.
+                - ``FTR_INVALIDINPUT``: First item is missing, or last
+                  call was already the last.
+        
+        See Also
+        --------
+        setup_list
+        abort_list
+
+        """
         response = self._request(
             23307,
             [next],
@@ -88,6 +196,21 @@ class TPS1200PFTR(GeoComSubsystem):
         return response
 
     def abort_list(self) -> GeoComResponse:
+        """
+        RPC 23308, ``FTR_AbortList``
+
+        Aborts current file listing setup.
+
+        Returns
+        -------
+        GeoComResponse
+        
+        See Also
+        --------
+        setup_list
+        list
+
+        """
         return self._request(23308)
 
     def setup_download(
@@ -97,6 +220,42 @@ class TPS1200PFTR(GeoComSubsystem):
         device: DEVICETYPE | str = DEVICETYPE.PCPARD,
         filetype: FILETYPE | str = FILETYPE.UNKNOWN,
     ) -> GeoComResponse:
+        """
+        RPC 23303, ``FTR_SetupDownload``
+
+        Prepares download of the specified file of the specified type, on 
+        the selected memory device.
+
+        Parameters
+        ----------
+        filename : str
+            File name (or full path if type is unknown).
+        blocksize : int
+            Download data block size.
+        device : DEVICETYPE | str, optional
+            Memory device, by default PCPARD
+        filetype : FILETYPE | str, optional
+            File type, by default UNKNOWN
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **blockcount** (`int`): Number of download blocks needed.
+            - Error codes:
+                - ``IVPARAM``: Memory device unavailable, or cannot find
+                  file path.
+                - ``NOTOK``: Setup already exists, previous setup was not
+                  aborted.
+                - ``FTR_INVALIDINPUT``: Block size too big.
+                - ``FTR_FILEACCESS``: File access error.
+        
+        See Also
+        --------
+        download
+        abort_download
+
+        """
         _device = toenum(self.DEVICETYPE, device)
         _filetype = toenum(self.FILETYPE, filetype)
         return self._request(
@@ -111,6 +270,34 @@ class TPS1200PFTR(GeoComSubsystem):
         self,
         block: int
     ) -> GeoComResponse:
+        """
+        RPC 23304, ``FTR_Download``
+
+        Downloads a single data block of a previously defined download
+        sequence.
+
+        Parameters
+        ----------
+        block : int
+            Number of data block to download.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **value** (`str`): Data block as serialized bytes.
+                - **length** (`int`): Length of data block.
+            - Error codes:
+                - ``FTR_MISSINGSETUP``: No active download setup.
+                - ``FTR_INVALIDINPUT``: First block is missing.
+                - ``FTR_FILEACCESS``: File access error.
+        
+        See Also
+        --------
+        setup_download
+        abort_download
+
+        """
         return self._request(
             23304,
             [block],
@@ -121,6 +308,21 @@ class TPS1200PFTR(GeoComSubsystem):
         )
 
     def abort_download(self) -> GeoComResponse:
+        """
+        RPC 23305, ``FTR_AbortDownload``
+
+        Aborts current file download setup.
+
+        Returns
+        -------
+        GeoComResponse
+        
+        See Also
+        --------
+        setup_download
+        download
+
+        """
         return self._request(23305)
 
     def delete(
@@ -130,6 +332,38 @@ class TPS1200PFTR(GeoComSubsystem):
         device: DEVICETYPE | str = DEVICETYPE.PCPARD,
         filetype: FILETYPE | str = FILETYPE.UNKNOWN
     ) -> GeoComResponse:
+        """
+        RPC 23309, ``FTR_Delete``
+
+        Deletes one or more files. Wildcards can be used to delete
+        multiple items. If a date is given, only files older than that
+        date are deleted.
+
+        Parameters
+        ----------
+        filename : str
+            File name (or full path if type is unknown).
+        time : datetime | None, optional
+            Deletion limit date, by default None
+        device : DEVICETYPE | str, optional
+            Memory device, by default PCPARD
+        filetype : FILETYPE | str, optional
+            File type, by default UNKNOWN
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **deleted** (`int`): Number of files deleted.
+            - Error codes:
+                - ``IVPARAM``: Memory device unavailable, or cannot find
+                  file path.
+        
+        See Also
+        --------
+        list
+
+        """
         _device = toenum(self.DEVICETYPE, device)
         _filetype = toenum(self.FILETYPE, filetype)
         if time is None:
