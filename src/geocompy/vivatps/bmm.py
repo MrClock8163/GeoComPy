f3a9d8e8d58aa367792e510908b30eb92c643aae
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 16:57:09 +0200

    VivaTPSBMM documentation

--- src/geocompy/vivatps/bmm.py
+++ src/geocompy/vivatps/bmm.py
@@ -1,7 +1,25 @@
+"""
+``geocompy.vivatps.bmm``
+=========================
+
+Definitions for the VivaTPS Basic man-machine interface subsystem.
+
+Types
+-----
+
+- ``VivaTPSBMM``
+
+"""
 from __future__ import annotations
 
 from ..tps1200p.bmm import TPS1200PBMM
 
 
 class VivaTPSBMM(TPS1200PBMM):
-    ...
+    """
+    Basic man-machine interface subsystem of the VivaTPS GeoCom protocol.
+
+    This subsystem contains functions related to the operation of the
+    keyboard, character sets and singalling devices.
+
+    """
