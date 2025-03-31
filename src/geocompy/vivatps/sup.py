e01048be6201c1efe54260c79587a07690278732
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 17:18:35 +0200

    VivaTPSSUP documentation

--- src/geocompy/vivatps/sup.py
+++ src/geocompy/vivatps/sup.py
@@ -1,3 +1,15 @@
+"""
+``geocompy.vivatps.sup``
+=========================
+
+Definitions for the VivaTPS Supervisor subsystem.
+
+Types
+-----
+
+- ``VivaTPSSUP``
+
+"""
 from __future__ import annotations
 
 from .. import GeoComResponse
@@ -5,10 +17,35 @@ from ..tps1200p.sup import TPS1200PSUP
 
 
 class VivaTPSSUP(TPS1200PSUP):
+    """
+    Supervisor subsystem of the VivaTPS GeoCom protocol.
+
+    This subsystem controls the continuous operation of the system, and it
+    allows to automatically display status information.
+
+    """
     def set_power_fail_autorestart(
         self,
         autorestart: bool
     ) -> GeoComResponse:
+        """
+        RPC 14006, ``SUP_SetPowerFailAutoRestart``
+
+        Configure the instrument to automatically restard if power is
+        restored after an irregular shutdown.
+
+        Parameters
+        ----------
+        autorestart : bool
+            Enable automatic restart.
+
+        Returns
+        -------
+        GeoComResponse
+            - Error codes:
+                - ``NA``: Command not available.
+
+        """
         return self._request(
             14006,
             [autorestart]
