fb51bd72a78d481db37685812fa149de35148308
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 16:55:53 +0200

    VivaTPSBAP documentation

--- src/geocompy/vivatps/bap.py
+++ src/geocompy/vivatps/bap.py
@@ -1,3 +1,15 @@
+"""
+``geocompy.vivatps.bap``
+=========================
+
+Definitions for the VivaTPS Basic applications subsystem.
+
+Types
+-----
+
+- ``VivaTPSBAP``
+
+"""
 from __future__ import annotations
 
 from enum import Enum
@@ -8,15 +20,51 @@ from ..tps1200p.bap import TPS1200PBAP
 
 
 class VivaTPSBAP(TPS1200PBAP):
+    """
+    Basic applications subsystem of the VivaTPS GeoCom protocol.
+
+    This subsystem contains high-level functions that are also accessible
+    through the user interface. The commands combine several subcommands
+    for ease of operation.
+
+    """
     class ONOFF(Enum):
         @classmethod
         def parse(cls, value: str) -> VivaTPSBAP.ONOFF:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~VivaTPSBAP.ONOFF
+                Parsed enum member.
+            """
             return cls(int(value))
 
         OFF = 0
         ON = 1
     
     def get_atr_precise(self) -> GeoComResponse:
+        """
+        RPC 17039, ``BAP_GetATRPrecise``
+
+        Gets the current state of the precise ATR mode.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **state** (`ONOFF`): Current state of precise ATR mode.
+
+        See Also
+        --------
+        set_atr_precise
+        """
         return self._request(
             17039,
             parsers={
@@ -28,6 +76,24 @@ class VivaTPSBAP(TPS1200PBAP):
         self,
         state: ONOFF | str
     ) -> GeoComResponse:
+        """
+        RPC 17040, ``BAP_SetATRPrecise``
+
+        Sets the state of the precise ATR mode.
+
+        Parameters
+        ----------
+        state : ONOFF | str
+            Precise ATR mode state to set.
+
+        Returns
+        -------
+        GeoComResponse
+
+        See Also
+        --------
+        get_atr_precise
+        """
         _state = toenum(self.ONOFF, state)
         return self._request(
             17040,
