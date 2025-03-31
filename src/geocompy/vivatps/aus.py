ca826cef96aba0fb4877f94f3555baccf3cfbf64
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 16:34:01 +0200

    VivaTPSAUS documentation

--- src/geocompy/vivatps/aus.py
+++ src/geocompy/vivatps/aus.py
@@ -1,7 +1,25 @@
+"""
+``geocompy.vivatps.aus``
+=========================
+
+Definitions for the VivaTPS Alt user subsystem.
+
+Types
+-----
+
+- ``VivaTPSAUS``
+
+"""
 from __future__ import annotations
 
 from ..tps1200p.aus import TPS1200PAUS
 
 
 class VivaTPSAUS(TPS1200PAUS):
-    pass
+    """
+    Alt user subsystem of the VivaTPS GeoCom protocol.
+
+    This subsystem can be used to set and query the ATR and LOCK
+    automation modes.
+
+    """
