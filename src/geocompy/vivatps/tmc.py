f54d5f0643384e1cc5df4f70b9adb4f11a6fe7c3
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 19:17:23 +0200

    VivaTPSTMC documentation

--- src/geocompy/vivatps/tmc.py
+++ src/geocompy/vivatps/tmc.py
@@ -1,15 +1,43 @@
+"""
+``geocompy.vivatps.tmc``
+=========================
+
+Definitions for the VivaTPS Theodolite measurement and calculation
+subsystem.
+
+Types
+-----
+
+- ``VivaTPSTMC``
+
+"""
 from __future__ import annotations
 
-from .. import GeoComResponse
 from ..tps1200p.tmc import TPS1200PTMC
 
 
 class VivaTPSTMC(TPS1200PTMC):
-    def set_prism_corr(
-        self,
-        const: float
-    ) -> GeoComResponse:
-        return self._request(
-            2024,
-            [const]
-        )
+    """
+    Theodolite measurement and calculation subsystem of the VivaTPS
+    GeoCom protocol.
+
+    This subsystem is the central module of measurement, calculation and
+    geodetic control.
+
+    The module handles:
+        - measurement functions
+        - measurement control functions
+        - data setup functions
+        - information functions
+        - configuration functions
+    
+    Possible return codes:
+        System
+            General use codes.
+        Informative/Warning
+            Function terminated with success, but some restrictions may
+            apply (e.g.: angle measurement succeded, distance measurement
+            failed).
+        Error
+            Non-successful function termination."
+    """
