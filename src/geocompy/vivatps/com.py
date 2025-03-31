1195ee3ff988bee9b7e73e32bf4da6f2a42c3ea6
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 16:59:52 +0200

    VivaTPSCOM documentation

--- src/geocompy/vivatps/com.py
+++ src/geocompy/vivatps/com.py
@@ -1,3 +1,15 @@
+"""
+``geocompy.vivatps.com``
+=========================
+
+Definitions for the VivaTPS Communication subsystem.
+
+Types
+-----
+
+- ``VivaTPSCOM``
+
+"""
 from __future__ import annotations
 
 from enum import Enum
@@ -6,9 +18,29 @@ from ..tps1200p.com import TPS1200PCOM
 
 
 class VivaTPSCOM(TPS1200PCOM):
+    """
+    Communication subsystem of the VivaTPS GeoCom protocol.
+
+    This subsystem contains functions relevant to the communication
+    with the instrument.
+
+    """
     class STOPMODE(Enum):
         @classmethod
         def parse(cls, value: str) -> VivaTPSCOM.STOPMODE:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~VivaTPSCOM.STOPMODE
+                Parsed enum member.
+            """
             return cls(int(value))
 
         SHUTDOWN = 0
@@ -18,6 +50,19 @@ class VivaTPSCOM(TPS1200PCOM):
     class STARTUPMODE(Enum):
         @classmethod
         def parse(cls, value: str) -> VivaTPSCOM.STARTUPMODE:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~VivaTPSCOM.STARTUPMODE
+                Parsed enum member.
+            """
             return cls(int(value))
 
         LOCAL = 0
