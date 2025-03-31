168540927d05e0f508b097b4464fea473930152b
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 20:55:19 +0200

    VivaTPSCSV documentation

--- src/geocompy/vivatps/csv.py
+++ src/geocompy/vivatps/csv.py
@@ -1,3 +1,15 @@
+"""
+``geocompy.vivatps.csv``
+=========================
+
+Definitions for the VivaTPS Central services subsystem.
+
+Types
+-----
+
+- ``VivaTPSCSV``
+
+"""
 from __future__ import annotations
 
 from enum import Enum
@@ -8,9 +20,29 @@ from ..tps1200p.csv import TPS1200PCSV
 
 
 class VivaTPSCSV(TPS1200PCSV):
+    """
+    Central services subsystem of the VivaTPS GeoCom protocol.
+
+    This subsystem contains functions to maintain centralised data
+    and configuration of the instruments.
+
+    """
     class ONOFF(Enum):
         @classmethod
         def parse(cls, value: str) -> VivaTPSCSV.ONOFF:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~VivaTPSCSV.ONOFF
+                Parsed enum member.
+            """
             return cls(int(value))
 
         OFF = 0
@@ -19,46 +51,72 @@ class VivaTPSCSV(TPS1200PCSV):
     class DEVICECLASS(Enum):
         @classmethod
         def parse(cls, value: str) -> VivaTPSCSV.DEVICECLASS:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~VivaTPSCSV.DEVICECLASS
+                Parsed enum member.
+            """
             return cls(int(value))
 
-        CLASS_1100 = 0  # TPS1000 3"
-        CLASS_1700 = 1  # TPS1000 1.5"
-        CLASS_1800 = 2  # TPS1000 1"
-        CLASS_5000 = 3  # TPS2000
-        CLASS_6000 = 4  # TPS2000
-        CLASS_1500 = 5  # TPS1000
-        CLASS_2003 = 6  # TPS2000
-        CLASS_5005 = 7  # TPS5000
-        CLASS_5100 = 8  # TPS5000
-        CLASS_1102 = 100  # TPS1100 2"
-        CLASS_1103 = 101  # TPS1100 3"
-        CLASS_1105 = 102  # TPS1100 5"
-        CLASS_1101 = 103  # TPS1100 1"
-        CLASS_1202 = 200  # TPS1200 2"
-        CLASS_1203 = 201  # TPS1200 3"
-        CLASS_1205 = 202  # TPS1200 5"
-        CLASS_1201 = 203  # TPS1200 1"
-        CLASS_Tx30 = 300  # TS30, MS30 0.5"
-        CLASS_Tx31 = 301  # TS30, MS30 1"
-        CLASS_TDRA = 350  # TDRA 0.5"
-        CLASS_TS01 = 500  # 1"
-        CLASS_TS02 = 501  # 2"
-        CLASS_TS03 = 502  # 3"
-        CLASS_TS05 = 503  # 5"
-        CLASS_TS06 = 504  # 6"
-        CLASS_TS07 = 505  # 7"
-        CLASS_TS10 = 506  # 10"
-        CLASS_TS1X_1 = 600  # Viva 1"
-        CLASS_TS1X_2 = 601  # Viva 2"
-        CLASS_TS1X_3 = 602  # Viva 3"
-        CLASS_TS1X_4 = 603  # Viva 4"
-        CLASS_TS1X_5 = 604  # Viva 5"
-        CLASS_TS50_05 = 650  # TPS1300 TS50/TM50, 0.5"
-        CLASS_TS50_1 = 651  # TPS1300 TS50/TM50, 1"
+        CLASS_1100 = 0  #: TPS1000 3"
+        CLASS_1700 = 1  #: TPS1000 1.5"
+        CLASS_1800 = 2  #: TPS1000 1"
+        CLASS_5000 = 3  #: TPS2000
+        CLASS_6000 = 4  #: TPS2000
+        CLASS_1500 = 5  #: TPS1000
+        CLASS_2003 = 6  #: TPS2000
+        CLASS_5005 = 7  #: TPS5000
+        CLASS_5100 = 8  #: TPS5000
+        CLASS_1102 = 100  #: TPS1100 2"
+        CLASS_1103 = 101  #: TPS1100 3"
+        CLASS_1105 = 102  #: TPS1100 5"
+        CLASS_1101 = 103  #: TPS1100 1"
+        CLASS_1202 = 200  #: TPS1200 2"
+        CLASS_1203 = 201  #: TPS1200 3"
+        CLASS_1205 = 202  #: TPS1200 5"
+        CLASS_1201 = 203  #: TPS1200 1"
+        CLASS_Tx30 = 300  #: TS30, MS30 0.5"
+        CLASS_Tx31 = 301  #: TS30, MS30 1"
+        CLASS_TDRA = 350  #: TDRA 0.5"
+        CLASS_TS01 = 500  #: 1"
+        CLASS_TS02 = 501  #: 2"
+        CLASS_TS03 = 502  #: 3"
+        CLASS_TS05 = 503  #: 5"
+        CLASS_TS06 = 504  #: 6"
+        CLASS_TS07 = 505  #: 7"
+        CLASS_TS10 = 506  #: 10"
+        CLASS_TS1X_1 = 600  #: Viva 1"
+        CLASS_TS1X_2 = 601  #: Viva 2"
+        CLASS_TS1X_3 = 602  #: Viva 3"
+        CLASS_TS1X_4 = 603  #: Viva 4"
+        CLASS_TS1X_5 = 604  #: Viva 5"
+        CLASS_TS50_05 = 650  #: TPS1300 TS50/TM50, 0.5"
+        CLASS_TS50_1 = 651  #: TPS1300 TS50/TM50, 1"
     
     class REFLESSCLASS(Enum):
         @classmethod
         def parse(cls, value: str) -> VivaTPSCSV.REFLESSCLASS:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~VivaTPSCSV.REFLESSCLASS
+                Parsed enum member.
+            """
             return cls(int(value))
 
         NONE = 0
@@ -71,6 +129,19 @@ class VivaTPSCSV(TPS1200PCSV):
     class POWERSOURCE(Enum):
         @classmethod
         def parse(cls, value: str) -> VivaTPSCSV.POWERSOURCE:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~VivaTPSCSV.POWERSOURCE
+                Parsed enum member.
+            """
             return cls(int(value))
 
         CURRENT = 0
@@ -80,6 +151,19 @@ class VivaTPSCSV(TPS1200PCSV):
     class PROPERTY(Enum):
         @classmethod
         def parse(cls, value: str) -> VivaTPSCSV.PROPERTY:
+            """
+            Parses enum member from serialized enum value.
+
+            Parameters
+            ----------
+            value : str
+                Serialized enum value.
+
+            Returns
+            -------
+            ~VivaTPSCSV.PROPERTY
+                Parsed enum member.
+            """
             return cls(int(value))
         
         PURCHASE_MODE_NORMAL = 0
@@ -120,12 +204,40 @@ class VivaTPSCSV(TPS1200PCSV):
         self,
         enabled: bool
     ) -> GeoComResponse:
+        """
+        RPC 5155, ``CSV_SetStartupMessageMode``
+
+        Enables or disables the startup message mode on the instrument.
+
+        Parameters
+        ----------
+        enabled : bool
+            Startup message mode is enabled.
+
+        Returns
+        -------
+        GeoComResponse
+
+        """
         return self._request(
             5155,
             [enabled]
         )
     
     def get_startup_message_mode(self) -> GeoComResponse:
+        """
+        RPC 5156, ``CSV_GetStartupMessageMode``
+
+        Gets the current status of the startup message mode on the
+        instrument.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **enabled** (`bool`): Startup message is enabled.
+
+        """
         return self._request(
             5156,
             parsers={
@@ -137,6 +249,21 @@ class VivaTPSCSV(TPS1200PCSV):
         self,
         state: ONOFF | str
     ) -> GeoComResponse:
+        """
+        RPC 5043, ``CSV_SwitchLaserlot``
+
+        Sets the state of the laserlot.
+
+        Parameters
+        ----------
+        state : ONOFF
+            State to set for laserlot.
+
+        Returns
+        -------
+        GeoComResponse
+
+        """
         _state = toenum(self.ONOFF, state)
         return self._request(
             5043,
@@ -144,6 +271,18 @@ class VivaTPSCSV(TPS1200PCSV):
         )
     
     def get_laserlot_status(self) -> GeoComResponse:
+        """
+        RPC 5042, ``CSV_GetLaserlotStatus``
+
+        Gets the current state of the laserlot.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **enabled** (`bool`): Startup message mode is enabled.
+
+        """
         return self._request(
             5042,
             parsers={
@@ -155,12 +294,39 @@ class VivaTPSCSV(TPS1200PCSV):
         self,
         intensity: int
     ) -> GeoComResponse:
+        """
+        RPC 5040, ``CSV_SetLaserlotIntens``
+
+        Sets the intensity of the laserlot.
+
+        Parameters
+        ----------
+        intensity : int
+            New laserlot intensity to set.
+
+        Returns
+        -------
+        GeoComResponse
+
+        """
         return self._request(
             5040,
             [intensity]
         )
     
     def get_laserlot_intensity(self) -> GeoComResponse:
+        """
+        RPC 5041, ``CSV_GetLaserlotIntens``
+
+        Gets the current intensity of the laserlot.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **intensity** (`int`): Current laserlot intensity.
+
+        """
         return self._request(
             5041,
             parsers={
@@ -172,6 +338,23 @@ class VivaTPSCSV(TPS1200PCSV):
         self,
         property: PROPERTY | str
     ) -> GeoComResponse:
+        """
+        RPC 5039, ``CSV_CheckProperty``
+
+        Checks if a specific license is available on the instrument.
+
+        Parameters
+        ----------
+        property : PROPERTY | str
+            License to check.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **available** (`bool`): License is available.
+
+        """
         _prop = toenum(self.PROPERTY, property)
         return self._request(
             5139,
@@ -182,10 +365,22 @@ class VivaTPSCSV(TPS1200PCSV):
         )
     
     def get_voltage(self) -> GeoComResponse:
+        """
+        RPC 5165, ``CSV_GetVoltage``
+
+        Gets the instrument voltage.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **voltage** (`int`): Instrument voltage [mV].
+
+        """
         return self._request(
             5165,
             parsers={
-                "millivolts": int
+                "voltage": int
             }
         )
     
@@ -193,6 +388,21 @@ class VivaTPSCSV(TPS1200PCSV):
         self,
         state: ONOFF | str
     ) -> GeoComResponse:
+        """
+        RPC 5161, ``CSV_SetCharging``
+
+        Sets the state of the charger.
+
+        Parameters
+        ----------
+        state : ONOFF | str
+            New state to set for charger.
+
+        Returns
+        -------
+        GeoComResponse
+
+        """
         _state = toenum(self.ONOFF, state)
         return self._request(
             5161,
@@ -200,6 +410,18 @@ class VivaTPSCSV(TPS1200PCSV):
         )
 
     def get_charging(self) -> GeoComResponse:
+        """
+        RPC 5162, ``CSV_GetCharging``
+
+        Gets the current state of the charger.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **state** (`ONOFF`): Current state of the charger.
+
+        """
         return self._request(
             5162,
             parsers={
@@ -211,6 +433,17 @@ class VivaTPSCSV(TPS1200PCSV):
         self,
         source: POWERSOURCE | str
     ) -> GeoComResponse:
+        """
+        RPC 5163, ``CSV_SetPreferredPowersource``
+
+        Sets the preferred power source.
+
+        Parameters
+        ----------
+        source : POWERSOURCE | str
+            New preferred power source to set.
+
+        """
         _source = toenum(self.POWERSOURCE, source)
         return self._request(
             5163,
@@ -218,6 +451,18 @@ class VivaTPSCSV(TPS1200PCSV):
         )
 
     def get_preferred_powersource(self) -> GeoComResponse:
+        """
+        RPC 5164, ``CSV_GetPreferredPowersource``
+
+        Gets the current preferred power source.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **source** (`POWERSOURCE`): Preferred power source.
+
+        """
         return self._request(
             5164, # Mistyped as 5163 in the GeoCom reference
             parsers={
