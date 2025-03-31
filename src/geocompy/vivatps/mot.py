4416f2c85862d457d398fd2208ad4f579bd8f965
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 17:15:20 +0200

    VivaTPSMOT documentation

--- src/geocompy/vivatps/mot.py
+++ src/geocompy/vivatps/mot.py
@@ -1,7 +1,24 @@
+"""
+``geocompy.vivatps.mot``
+=========================
+
+Definitions for the VivaTPS Motorization subsystem.
+
+Types
+-----
+
+- ``VivaTPSMOT``
+
+"""
 from __future__ import annotations
 
 from ..tps1200p.mot import TPS1200PMOT
 
 
 class VivaTPSMOT(TPS1200PMOT):
-    pass
+    """
+    Motorization subsystem of the VivaTPS GeoCom protocol.
+
+    This subsystem provides access to motoriztaion parameters and control.
+
+    """
