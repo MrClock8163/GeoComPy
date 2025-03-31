46bd7ae492c37a344ddb66a48da7fcd3a37fb45e
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-03-31 19:24:56 +0200

    VivaTPSKDM documentation

--- src/geocompy/vivatps/kdm.py
+++ src/geocompy/vivatps/kdm.py
@@ -1,3 +1,15 @@
+"""
+``geocompy.vivatps.kdm``
+=========================
+
+Definitions for the VivaTPS Keyboard display unit subsystem.
+
+Types
+-----
+
+- ``VivaTPSKDM``
+
+"""
 from __future__ import annotations
 
 from .. import (
@@ -6,17 +18,51 @@ from .. import (
 )
 
 
-class VivaTPSKDM(GeoComSubsystem):    
+class VivaTPSKDM(GeoComSubsystem):
+    """
+    Keyboard display unit subsystem of the VivaTPS GeoCom protocol.
+
+    This subsystem controls the keyboard and display functions.
+
+    """
     def set_lcd_power(
         self,
         alwayson: bool
     ) -> GeoComResponse:
+        """
+        RPC 23107, ``KDM_SetLcdPower``
+
+        Sets the status of the diplay power.
+
+        Parameters
+        ----------
+        alwayson : bool
+            Keep display turned on, do not go into screensaver mode.
+
+        Returns
+        -------
+        GeoComResponse
+
+        """
         return self._request(
             23107,
             [alwayson]
         )
     
     def get_lcd_power(self) -> GeoComResponse:
+        """
+        RPC 23108, ``KDM_GetLcdPower``
+
+        Gets the current status of the diplay power.
+
+        Returns
+        -------
+        GeoComResponse
+            - Params:
+                - **alwayson** (`bool`): Keep display turned on, do not go
+                  into screensaver mode.
+
+        """
         return self._request(
             23108,
             parsers={
