2188e6795414039652e188000bbc7bf5ff66a0d2
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-04-22 16:51:36 +0200

    Added basic tests for VivaTPS

---
+++ tests/test_vivatps.py
@@ -0,0 +1,31 @@
+import pytest
+
+from geocompy.communication import Connection
+from geocompy.vivatps import VivaTPS
+
+from helpers_geocom import (
+    DummyGeoComConnection,
+    GeoComTester
+)
+
+
+@pytest.fixture
+def tps() -> VivaTPS:
+    return VivaTPS(DummyGeoComConnection())
+
+
+class TestVivaTPS:
+    def test_init(self):
+        conn_bad = Connection()
+        with pytest.raises(ConnectionError):
+            VivaTPS(conn_bad, retry=1)
+
+        conn_good = DummyGeoComConnection()
+        instrument = VivaTPS(conn_good)
+        assert instrument._precision == 15
+
+    def test_parse_response(self, tps: VivaTPS):
+        GeoComTester.test_parse_response(tps)
+
+    def test_request(self, tps: VivaTPS):
+        GeoComTester.test_request(tps)
