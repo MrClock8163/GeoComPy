124b2a171c6ae58e0f56d73491f50ad49721577c
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-05-02 12:10:54 +0200

    Rebased TPS1200P AUS

--- docs/api/tps1200p/aus.rst
+++ docs/api/tps1200p/aus.rst
@@ -3,6 +3,7 @@ Alt-User
 
 .. automodule:: geocompy.tps1200p.aus
     :inherited-members:
+    :exclude-members: get_rcs_search_switch,switch_rcs_search
 
     Definitions
     -----------
--- src/geocompy/tps1200p/aus.py
+++ src/geocompy/tps1200p/aus.py
@@ -14,19 +14,13 @@ Types
 """
 from __future__ import annotations
 
-from enum import Enum
+from typing import Never
+from typing_extensions import deprecated
 
-from ..data import (
-    toenum,
-    enumparser
-)
-from ..protocols import (
-    GeoComSubsystem,
-    GeoComResponse
-)
+from ..tps1100.aus import TPS1100AUS
 
 
-class TPS1200PAUS(GeoComSubsystem):
+class TPS1200PAUS(TPS1100AUS):
     """
     Alt user subsystem of the TPS1200+ GeoCom protocol.
 
@@ -34,132 +28,32 @@ class TPS1200PAUS(GeoComSubsystem):
     automation modes.
 
     """
-    class ONOFF(Enum):
-        OFF = 0
-        ON = 1
 
-    def get_user_atr_state(self) -> GeoComResponse[ONOFF]:
+    @deprecated("This command was removed for TPS1200 instruments")
+    def get_rcs_search_switch(self) -> Never:
         """
-        RPC 18006, ``AUS_GetUserAtrState``
+        RPC 18010, ``AUS_GetRcsSearchSwitch``
 
-        Gets the current state of the ATR mode.
+        .. versionremoved:: GeoCom-TPS1200
 
-        Returns
-        -------
-        GeoComResponse
-            Params:
-                - `ONOFF`: current ATR state
-
-            Error codes:
-                - ``NOT_IMPL``: ATR is not available.
-
-        Notes
-        -----
-        This command does not indicate if the ATR has acquired a prism.
-
-        See Also
-        --------
-        set_user_atr_state
+        Raises
+        ------
+        AttributeError
         """
-        return self._request(
-            18006,
-            parsers=enumparser(self.ONOFF)
-        )
+        raise AttributeError()
 
-    def set_user_atr_state(
+    @deprecated("This command was removed for TPS1200 instruments")
+    def switch_rcs_search(
         self,
-        state: ONOFF | str
-    ) -> GeoComResponse[None]:
+        *args
+    ) -> Never:
         """
-        RPC 18005, ``AUS_SetUserAtrState``
+        RPC 18009, ``AUS_SwitchRcsSearch``
 
-        Activates or deactivates the ATR mode.
+        .. versionremoved:: GeoCom-TPS1200
 
-        Parameters
-        ----------
-        state : ONOFF | str
-            ATR state to set
-
-        Returns
-        -------
-        GeoComResponse
-            Error codes:
-                - ``NOT_IMPL``: ATR is not available.
-
-        Notes
-        -----
-        If LOCK mode is active when the ATR is activated, then LOCK mode
-        changes to ATR mode.
-
-        If the ATR is deactivated, the LOCK mode does not change.
-
-        See Also
-        --------
-        get_user_atr_state
-        get_user_lock_state
-        set_user_lock_state
+        Raises
+        ------
+        AttributeError
         """
-        _state = toenum(self.ONOFF, state)
-        return self._request(18005, [_state.value])
-
-    def get_user_lock_state(self) -> GeoComResponse[ONOFF]:
-        """
-        RPC 18005, ``AUS_GetUserLockState``
-
-        Gets the current state of the LOCK mode.
-
-        Returns
-        -------
-        GeoComResponse
-            Params:
-                - `ONOFF`: current ATR state
-
-            Error codes:
-                - ``NOT_IMPL``: ATR is not available.
-
-        See Also
-        --------
-        set_user_lock_state
-        mot.read_lock_status
-        """
-        return self._request(
-            18008,
-            parsers=enumparser(self.ONOFF)
-        )
-
-    def set_user_lock_state(
-        self,
-        state: ONOFF | str
-    ) -> GeoComResponse[None]:
-        """
-        RPC 18007, ``AUS_SetUserLockState``
-
-        Activates or deactivates the LOCK mode.
-
-        Parameters
-        ----------
-        state : ONOFF | str
-            LOCK state to set
-
-        Returns
-        -------
-        GeoComResponse
-            Error codes:
-                - ``NOT_IMPL``: ATR is not available.
-
-        Notes
-        -----
-        Activating the LOCK mode does not mean that the instrument is
-        automatically locked onto a prism.
-
-        See Also
-        --------
-        get_user_lock_state
-        get_user_atr_state
-        aut.lock_in
-        """
-        _state = toenum(self.ONOFF, state)
-        return self._request(
-            18007,
-            [_state.value]
-        )
+        raise AttributeError()
