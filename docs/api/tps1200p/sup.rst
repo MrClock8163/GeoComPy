30fd44eda24f50c181f5292d2b93e1c7253d0a66
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-05-02 13:34:49 +0200

    Rebased TPS1200 SUP

--- docs/api/tps1200p/sup.rst
+++ docs/api/tps1200p/sup.rst
@@ -3,6 +3,7 @@ Supervisor
 
 .. automodule:: geocompy.tps1200p.sup
     :inherited-members:
+    :exclude-members: switch_low_temp_control
 
     Definitions
     -----------
--- src/geocompy/tps1200p/sup.py
+++ src/geocompy/tps1200p/sup.py
@@ -14,19 +14,13 @@ Types
 """
 from __future__ import annotations
 
-from enum import Enum
+from typing import Never
+from typing_extensions import deprecated
 
-from ..data import (
-    toenum,
-    enumparser
-)
-from ..protocols import (
-    GeoComSubsystem,
-    GeoComResponse
-)
+from ..tps1100.sup import TPS1100SUP
 
 
-class TPS1200PSUP(GeoComSubsystem):
+class TPS1200PSUP(TPS1100SUP):
     """
     Supervisor subsystem of the TPS1200+ GeoCom protocol.
 
@@ -34,74 +28,19 @@ class TPS1200PSUP(GeoComSubsystem):
     allows to automatically display status information.
 
     """
-    class ONOFF(Enum):
-        OFF = 0
-        ON = 1
 
-    class AUTOPOWER(Enum):
-        DISABLED = 0  # : Automatic poweroff disabled.
-        OFF = 2  # : Poweroff instrument.
-
-    def get_config(self) -> GeoComResponse[tuple[ONOFF, AUTOPOWER, int]]:
-        """
-        RPC 14001, ``SUP_GetConfig``
-
-        Gets the current poweroff and timing configuration.
-
-        Returns
-        -------
-        GeoComResponse
-            Params:
-                - `ONOFF`: Reserved.
-                - `AUTOPOWER`: Current shutdown mechanism.
-                - `int`: Idling timeout [ms].
-
-        See Also
-        --------
-        set_config
-
-        """
-        return self._request(
-            14001,
-            parsers=(
-                enumparser(self.ONOFF),
-                enumparser(self.AUTOPOWER),
-                int
-            )
-        )
-
-    def set_config(
+    @deprecated("This command was removed for TPS1200 instruments")
+    def switch_low_temp_control(
         self,
-        autopower: AUTOPOWER | str = AUTOPOWER.OFF,
-        timeout: int = 600_000,
-        reserved: ONOFF | str = ONOFF.ON
-    ) -> GeoComResponse[None]:
+        *args
+    ) -> Never:
         """
-        RPC 14002, ``SUP_SetConfig``
+        RPC 14003, ``SUP_SwitchLowTempControl``
 
-        Sets the poweroff and timing configuration.
+        .. versionremoved:: GeoCom-TPS1200
 
-        Parameters
-        ----------
-        autopower : AUTOPOWER | str, optional
-            Automatic poweroff action.
-        timeout : int, optional
-            Idling timeout [60000, 6000000] [ms], by default 600000
-
-        Returns
-        -------
-        GeoComResponse
-            Error codes:
-                - ``IVPARAM``: Invalid timeout parameter.
-
-        See Also
-        --------
-        get_config
-
+        Raises
+        ------
+        AttributeError
         """
-        _autopower = toenum(self.AUTOPOWER, autopower)
-        _reserved = toenum(self.ONOFF, reserved)
-        return self._request(
-            14002,
-            [_reserved.value, _autopower.value, timeout]
-        )
+        raise AttributeError()
