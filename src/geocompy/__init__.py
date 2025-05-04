d734c2f9b1f7e37a02a96e674ac92d20ec013452
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2025-05-04 16:33:22 +0200

    Added reexports to top level module

--- src/geocompy/__init__.py
+++ src/geocompy/__init__.py
@@ -57,8 +57,79 @@ Submodules
 ``geocompy.protocols``
     Base definitions for command protocols and responses.
 
+Reexports
+---------
+
+``geocompy.data.Angle``
+    Angle value primitive.
+
+``geocompy.data.AngleUnit``
+    Angle unit enum.
+
+``geocompy.data.Vector``
+    3D vector primitive.
+
+``geocompy.data.Coordinate``
+    3D coordinate primitive.
+
+``geocompy.protocols.GeoComResponse``
+    GeoCom protocol response container.
+
+``geocompy.protocols.GsiOnlineResponse``
+    GSI Online protocol response container.
+
+``geocompy.dna.DNA``
+    DNA instrument implementation.
+
+``geocompy.tps1000.TPS1000``
+    TPS1000 instrument implementation.
+
+``geocompy.tps1000.rc.TPS1000RC``
+    TPS1000 GeoCom return codes.
+
+``geocompy.tps1100.TPS1100``
+    TPS1100 instrument implementation.
+
+``geocompy.tps1100.rc.TPS1100RC``
+    TPS1100 GeoCom return codes.
+
+``geocompy.tps1200p.TPS1200P``
+    TPS1200+ instrument implementation.
+
+``geocompy.tps1200p.grc.TPS1200PGRC``
+    TPS1200+ GeoCom return codes.
+
+``geocompy.vivatps.VivaTPS``
+    VivaTPS instrument implementation.
+
+``geocompy.vivatps.grc.VivaTPSGRC``
+    VivaTPS GeoCom return codes.
+
 """
 try:
     from ._version import __version__
 except Exception:
     __version__ = "0.0.0"  # Placeholder value for source installs
+
+from .data import (  # noqa: F401
+    Angle as Angle,
+    AngleUnit as AngleUnit,
+    Vector as Vector,
+    Coordinate as Coordinate
+)
+
+from .protocols import (  # noqa: F401
+    GeoComResponse as GeoComResponse,
+    GsiOnlineResponse as GsiOnlineResponse
+)
+
+from .dna import DNA as DNA  # noqa: F401
+
+from .tps1000 import TPS1000 as TPS1000  # noqa: F401
+from .tps1000.rc import TPS1000RC as TPS1000RC  # noqa: F401
+from .tps1100 import TPS1100 as TPS1100  # noqa: F401
+from .tps1100.rc import TPS1100RC as TPS1100RC  # noqa: F401
+from .tps1200p import TPS1200P as TPS1200P  # noqa: F401
+from .tps1200p.grc import TPS1200PGRC as TPS1200PGRC  # noqa: F401
+from .vivatps import VivaTPS as VivaTPS  # noqa: F401
+from .vivatps.grc import VivaTPSGRC as VivaTPSGRC  # noqa: F401
